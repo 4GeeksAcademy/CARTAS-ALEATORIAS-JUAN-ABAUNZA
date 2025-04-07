1f308b7e33b462f429bdabff2f4a01bcf747789f
JuanMa305 <jake_nel@hotmail.com>
2025-04-07 06:30:35 +0000

    90% terminado

--- src/app.js
+++ src/app.js
@@ -2,13 +2,16 @@ window.onload = function() {
   const values = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
   const suitSymbols = [ "♠", "♣", "♥", "♦" ];
   
+  
   const randomValue = values[Math.floor(Math.random() * values.length)];
   const randomSuitIndex = suitSymbols [Math.floor(Math.random() * suitSymbols.length)];
+  
 
   
   document.getElementById("top-left").innerText = randomSuitIndex;
   document.getElementById("center").innerText = randomValue;
   document.getElementById("bottom-right").innerText = randomSuitIndex;
+  
   
   //document.getElementById("card").classList.add(randomSuit);
 };
